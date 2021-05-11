23988d62cb6ea8aa81512f5f4fccc9d283376071
dennaz2 <dennabellman@gmail.com>
2021-05-11 18:15:06 -0400

    added img sizing for the cards. Implemented shuffled cards

--- shuffle.php
+++ shuffle.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Card shuffling</title>
-    <link rel="shortcut icon" href="assets/AS.png" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/AS.png" type="image/x-icon"> <!-- For browser icon-->
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -14,11 +14,25 @@
         $players = substr(filter_input(INPUT_POST, 'players', FILTER_SANITIZE_NUMBER_INT),0,1);
         $cards = substr(filter_input(INPUT_POST, 'cards', FILTER_SANITIZE_NUMBER_INT),0,1);
 
-        //Declare variables 
+        //Unshuffled deck
+        $unshuffledDeck = array("2H", "2D", "2C",  "2S", "3H", "3D", "3C", "3S", "4H", "4D", "4C", "4S", "5H", "5D", "5C", "5S", "6H", "6D", "6C", "6S", "7H", "7D", "7C", "7S", "8H", "8D", "8C", "8S", "9H", "9D", "9C", "9S", "10H", "10D", "10C", "10S", "JH", "JD", "JC", "JS", "QH", "QD", "QC", "QS", "KH", "KD", "KC", "KS", "AH", "AD", "AC", "AS");
+        $arrayAmount = count($unshuffledDeck);
+        
+        echo "<p>Unshuffled Deck: ";
+        for($i = 0; $i < $arrayAmount; $i++){
+            echo $unshuffledDeck[$i]." ";
+        }
+        echo " </p>";
 
-        $unsuffledDeck = array("2H", "2D", "2C",  "2S", "3H", "3D", "3C", "3S", "4H", "4D", "4C", "4S", "5H", "5D", "5C", "5S", "6H", "6D", "6C", "6S", "7H", "7D", "7C", "7S", "8H", "8D", "8C", "8S", "9H", "9D", "9C", "9S", "10H", "10D", "10C", "10S", "JH", "JD", "JC", "JS", "QH", "QD", "QC", "QS", "KH", "KD", "KC", "KS", "AH", "AD", "AC", "AS");
+        //Shuffle Deck 
+        shuffle($unshuffledDeck);
 
-        $arrayAmount = count($unsuffledDeck);
+        //Display shuffled deck
+        echo "<p>S huffled Deck: ";
+        for($i = 0; $i < $arrayAmount; $i++){
+            echo $unshuffledDeck[$i]." ";
+        }
+        echo " </p>";
 
         //Display a header 
         echo "<h4>Cards dealed to players: </h4>";
@@ -27,31 +41,7 @@
         echo "<p>Number of Players: $players</p>";
         echo "<p>Number of Cards: $cards</p>";
 
-        //functions
-        function shuffleDeck($unshuffledDeck, $arrayAmo){
-            $shuffledDecked = [];
-
-            while($arrayAmo){
-                //Pick random card 
-                $randNum = rand(1, $arrayAmo);
-
-                //add card to shuffled array 
-                array_push($shuffledDecked, $unshuffledDeck[$randNum]);
-
-                //splice card from unshuffles array
-                array_splice($unshuffledDeck, $unshuffledDeck[$randNum], 1);
-
-                echo "<p>$shuffledDecked</p>";
-
-                return shuffleDeck;
-            }
-
-        }
-
-        //Function call
-        shuffleDeck($unsuffledDeck, $arrayAmount);
-        
-
+        //Deal cards to players
 
     
     ?>    
@@ -61,4 +51,18 @@
 </body>
 </html>
 
-<!-- https://cislinux.hfcc.edu/~dbellman/cardShuffling/shuffle.php -->
+<!-- https://cislinux.hfcc.edu/~dbellman/cardShuffling/index.html -->
+
+<!-- //Shuffling the deck 
+       
+       while($shuffledDecked  < $arrayAmount){
+           //Pick random card 
+           $randNum = rand(1, $arrayAmount);
+
+           //add card to shuffled array 
+           array_push($shuffledDecked, $unshuffledDeck[$randNum]);
+
+           //splice card from unshuffles array
+           array_splice($unshuffledDeck, $randNum, 1);
+           // unset($unshuffledDeck[$randNum]);
+       } -->
