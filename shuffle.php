93de08f7595b552e74831d3eb08f35a98992e00a
dennaz2 <dennabellman@gmail.com>
2021-05-11 01:03:44 -0400

    added shuffling function, css margin and padding changes

--- shuffle.php
+++ shuffle.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Card shuffling</title>
+    <link rel="shortcut icon" href="assets/AS.png" type="image/x-icon">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <?php
@@ -13,7 +15,10 @@
         $cards = substr(filter_input(INPUT_POST, 'cards', FILTER_SANITIZE_NUMBER_INT),0,1);
 
         //Declare variables 
+
         $unsuffledDeck = array("2H", "2D", "2C",  "2S", "3H", "3D", "3C", "3S", "4H", "4D", "4C", "4S", "5H", "5D", "5C", "5S", "6H", "6D", "6C", "6S", "7H", "7D", "7C", "7S", "8H", "8D", "8C", "8S", "9H", "9D", "9C", "9S", "10H", "10D", "10C", "10S", "JH", "JD", "JC", "JS", "QH", "QD", "QC", "QS", "KH", "KD", "KC", "KS", "AH", "AD", "AC", "AS");
+
+        $arrayAmount = count($unsuffledDeck);
 
         //Display a header 
         echo "<h4>Cards dealed to players: </h4>";
@@ -22,7 +27,32 @@
         echo "<p>Number of Players: $players</p>";
         echo "<p>Number of Cards: $cards</p>";
 
-        //
+        //functions
+        function shuffleDeck($unshuffledDeck, $arrayAmo){
+            $shuffledDecked = [];
+
+            while($arrayAmo){
+                //Pick random card 
+                $randNum = rand(1, $arrayAmo);
+
+                //add card to shuffled array 
+                array_push($shuffledDecked, $unshuffledDeck[$randNum]);
+
+                //splice card from unshuffles array
+                array_splice($unshuffledDeck, $unshuffledDeck[$randNum], 1);
+
+                echo "<p>$shuffledDecked</p>";
+
+                return shuffleDeck;
+            }
+
+        }
+
+        //Function call
+        shuffleDeck($unsuffledDeck, $arrayAmount);
+        
+
+
     
     ?>    
 
